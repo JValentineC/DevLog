091821d4f576f220786fef90c6390b7d7020f126
Jonathan Ramirez <jramirez@icstars.org>
2026-03-02 14:05:38 -0600

    navbar ellipsis dropdown menu

--- jvc-dev-log/src/components/Header.tsx
+++ jvc-dev-log/src/components/Header.tsx
@@ -17,24 +17,31 @@ function Header() {
         <NavLink to="/" className="text-xl font-bold">DevLog</NavLink>
       </div>
       <nav aria-label="Main navigation" className="flex-none">
-        <ul className="menu menu-horizontal px-1 gap-1">
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/entries">Entries</NavLink></li>
-          {user && <li><NavLink to="/entries/new">New Entry</NavLink></li>}
-          <li><NavLink to="/about">About</NavLink></li>
-          {user ? (
-            <>
-              <li className="flex items-center"><em>{user.username}</em></li>
-              <li>
-                <button type="button" onClick={logout} className="btn btn-ghost btn-sm">
-                  Log Out
-                </button>
-              </li>
-            </>
-          ) : (
-            <li><NavLink to="/login">Log In</NavLink></li>
-          )}
-        </ul>
+        <div className="dropdown dropdown-end">
+          <button tabIndex={0} className="btn btn-square btn-ghost" aria-label="Open menu">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+            </svg>
+          </button>
+          <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-10 w-52 p-2 shadow-lg">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/entries">Entries</NavLink></li>
+            {user && <li><NavLink to="/entries/new">New Entry</NavLink></li>}
+            <li><NavLink to="/about">About</NavLink></li>
+            {user ? (
+              <>
+                <li className="menu-title"><em>{user.username}</em></li>
+                <li>
+                  <button type="button" onClick={logout}>
+                    Log Out
+                  </button>
+                </li>
+              </>
+            ) : (
+              <li><NavLink to="/login">Log In</NavLink></li>
+            )}
+          </ul>
+        </div>
       </nav>
     </header>
   )
